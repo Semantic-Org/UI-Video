d6ad377465a308358c7694d44f7effcd777016e3
Pusher Robot <jack@semantic-ui.com>
2015-05-04 13:00:02 -0400

    Updated component to version 1.12.2

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-video',
   summary : 'Semantic UI - Video: Single component release',
-  version : '1.12.1',
+  version : '1.12.2',
   git     : 'git://github.com/Semantic-Org/UI-Video.git',
 });
 
